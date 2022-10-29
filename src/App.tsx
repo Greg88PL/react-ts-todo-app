4d798dcb6485c7a2140648590282a2cdb8c9359f
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-10-29 13:16:40 +0200

    add list and deal with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  return <div></div>;
+  const todos = [
+    {
+      id: "id1",
+      text: "javascript",
+    },
+    {
+      id: "id2",
+      text: "typescript",
+    },
+    {
+      id: "id3",
+      text: "html",
+    },
+  ];
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
