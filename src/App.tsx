bac61fa65baf8ab8dd00171f8cd09f96364652f7
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-10-31 10:56:50 +0100

    add removing a todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
@@ -17,10 +17,16 @@ function App() {
     });
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
